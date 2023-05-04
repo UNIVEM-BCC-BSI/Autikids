df04c56c5ebb7e0ef6f583a2a729e8ca917533e6
Samuel Pereira Barreto <70921394+sampbrt@users.noreply.github.com>
2023-05-04 20:35:13 -0300

    Update cadastroAlunos.py
    baixa a fonte poppins pra funcionar

--- cadastroAlunos.py
+++ cadastroAlunos.py
@@ -2,7 +2,6 @@ import sqlite3
 import tkinter
 import os
 
-
 pastaApp=os.path.dirname(__file__)
 
 # conexao = sqlite3.connect('banco_alunos.db')
@@ -58,73 +57,64 @@ def exporta_alunos():
     conexao.commit()
     conexao.close()
 
-
 janela = tkinter.Tk()
 janela.title('Autikids')
 janela.iconbitmap('icon.ico')
-janela.geometry("800x500")
-janela.config(bg='orange')
+janela.config(bg='orange', bd=150)
+
 # labels:
 
-label_nome= tkinter.Label(janela, text='Nome',bg='orange',fg="black")
-label_nome.grid(row=0, column=0, padx=10, pady=10)
+label_titulo = tkinter.Label(janela, text='Cadastro de aluno')
+label_titulo.configure(font=('Poppins', 20, 'bold'), bg='orange', fg='white')
+label_titulo.grid(row=0, column=1, columnspan=1)
 
-label_sobrenome= tkinter.Label(janela, text='Sobrenome',bg='orange',fg="black")
-label_sobrenome.grid(row=1, column=0, padx=10, pady=10)
+label_nome= tkinter.Label(janela, text='Nome: ',bg='orange',fg="black",border=2, font=('Poppins 12 bold'))
+label_nome.grid(row=1, column=0, padx=10, pady=10, sticky='e')
 
-label_telefone= tkinter.Label(janela, text='Telefone',bg='orange',fg="black")
-label_telefone.grid(row=2, column=0, padx=10, pady=10)
+label_sobrenome= tkinter.Label(janela, text='Sobrenome: ',bg='orange',fg="black",border=2, font=('Poppins 12 bold'))
+label_sobrenome.grid(row=2, column=0, padx=10, pady=10, sticky='e')
 
-label_datadenascimento=tkinter.Label(janela,text='Data de Nascimento',bg='orange',fg='black')
-label_datadenascimento.grid(row=3,column=0,padx=10,pady=10)
+label_telefone= tkinter.Label(janela, text='Telefone: ',bg='orange',fg="black",border=2, font=('Poppins 12 bold'))
+label_telefone.grid(row=3, column=0, padx=10, pady=10, sticky='e')
 
-label_endereco= tkinter.Label(janela, text='Endereço',bg='orange',fg="black")
-label_endereco.grid(row=4, column=0, padx=10, pady=10)
+label_datadenascimento=tkinter.Label(janela,text='Data de Nascimento: ',bg='orange',fg='black',border=2, font=('Poppins 12 bold'))
+label_datadenascimento.grid(row=4,column=0,padx=10,pady=10, sticky='e')
 
-label_cep= tkinter.Label(janela, text='CEP',bg='orange',fg="black")
-label_cep.grid(row=5, column=0, padx=10, pady=10)
+label_endereco= tkinter.Label(janela, text='Endereço: ',bg='orange',fg="black",border=2, font=('Poppins 12 bold'))
+label_endereco.grid(row=5, column=0, padx=10, pady=10, sticky='e')
 
-label_bairro= tkinter.Label(janela, text='Bairro',bg='orange',fg="black")
-label_bairro.grid(row=6, column=0, padx=10, pady=10)
+label_cep= tkinter.Label(janela, text='CEP: ',bg='orange',fg="black",border=2, font=('Poppins 12 bold'))
+label_cep.grid(row=6, column=0, padx=10, pady=10, sticky='e')
+
+label_bairro= tkinter.Label(janela, text='Bairro: ',bg='orange',fg="black",border=2, font=('Poppins 12 bold'))
+label_bairro.grid(row=7, column=0, padx=10, pady=10, sticky='e')
 
 
 # Entrys:
 
-entry_nome= tkinter.Entry(janela, width=30)
-entry_nome.grid(row=0, column=1, padx=10, pady=10)
+entry_nome= tkinter.Entry(janela, width=30, border=2, font=('Poppins 12'))
+entry_nome.grid(row=1, column=1, pady=10)
 
-entry_sobrenome= tkinter.Entry(janela,width=30)
-entry_sobrenome.grid(row=1, column=1, padx=10, pady=10)
+entry_sobrenome= tkinter.Entry(janela,width=30, border=2, font=('Poppins 12'))
+entry_sobrenome.grid(row=2, column=1, pady=10)
 
-entry_telefone= tkinter.Entry(janela,width=30)
-entry_telefone.grid(row=2, column=1, padx=10, pady=10)
+entry_telefone= tkinter.Entry(janela,width=30, border=2, font=('Poppins 12'))
+entry_telefone.grid(row=3, column=1, pady=10)
 
-entry_datadenascimento=tkinter.Entry(janela,width=30)
-entry_datadenascimento.grid(row=3,column=1,padx=10,pady=10)
+entry_datadenascimento=tkinter.Entry(janela,width=30, border=2, font=('Poppins 12'))
+entry_datadenascimento.grid(row=4,column=1,pady=10)
 
+entry_endereco= tkinter.Entry(janela,width=30, border=2, font=('Poppins 12'))
+entry_endereco.grid(row=5, column=1, pady=10)
 
-entry_endereco= tkinter.Entry(janela,width=30)
-entry_endereco.grid(row=4, column=1, padx=10, pady=10)
+entry_cep= tkinter.Entry(janela,width=30, border=2, font=('Poppins 12'))
+entry_cep.grid(row=6, column=1, pady=10)
 
-entry_cep= tkinter.Entry(janela,width=30)
-entry_cep.grid(row=5, column=1, padx=10, pady=10)
-
-entry_bairro= tkinter.Entry(janela,width=30)
-entry_bairro.grid(row=6, column=1, padx=10, pady=10)
-
-
-
-
+entry_bairro= tkinter.Entry(janela,width=30, border=2, font=('Poppins 12'))
+entry_bairro.grid(row=7, column=1, pady=10)
 
 # Botoes:
-botao_cadastar= tkinter.Button(janela,text="Cadastrar Aluno", command= cadastrar_aluno)
-botao_cadastar.grid(row=8, column=0, padx=10, pady=10, columnspan=2, ipadx=80)
-
-
-
-
-
-
+botao_cadastar= tkinter.Button(janela,text="Cadastrar Aluno", command= cadastrar_aluno, border=2, font=('Poppins 12 bold'), activebackground='yellow')
+botao_cadastar.grid(row=8,column=1,pady=10, sticky='nswe')
 
 janela.mainloop()
-
